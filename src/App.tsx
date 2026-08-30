3af0f004d0decc07a47b6adad7d8441ddb151908
FrontEndDevo <mylinkacco@gmail.com>
2026-08-30 13:21:37 +0300

    Start our eCommerce App

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-3xl font-bold text-blue-600">
+          React + TypeScript + Tailwind v4
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
