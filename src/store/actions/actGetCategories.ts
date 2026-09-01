509a90e4d9d8bf436c66ca0de5908859ba42d7ff
FrontEndDevo <mylinkacco@gmail.com>
2026-09-01 13:05:58 +0300

    Create async thunk to fetch the categories data and handling any errors

---
+++ src/store/actions/actGetCategories.ts
@@ -0,0 +1,24 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import type { TCategory } from "../../types/category";
+
+const actGetCategories = createAsyncThunk(
+  "categories/actGetCategories",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      const response = await axios.get<TCategory[]>("");
+
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(error.response?.data.message || error.message);
+      } else {
+        return rejectWithValue("Unexepected error!");
+      }
+    }
+  },
+);
+
+export default actGetCategories;
