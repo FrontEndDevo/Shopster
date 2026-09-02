e7a460dc1be82f2e0a74e5f7f3ce2fd5a4ca4091
FrontEndDevo <mylinkacco@gmail.com>
2026-09-02 20:06:36 +0300

    Style the cart and quantity of items

--- src/components/ecommerce/ShoppingCart.tsx
+++ src/components/ecommerce/ShoppingCart.tsx
@@ -1,13 +1,16 @@
 import { getTotalCartQuantitySelector } from "@/store/cart/cartSlice";
 import { useAppSelector } from "@/store/hooks";
+import Cart from "@/assets/cart.svg?react";
 
 const ShoppingCart = () => {
   const totalQuantity = useAppSelector(getTotalCartQuantitySelector);
 
   return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <p>{totalQuantity}</p>
+    <div className="relative">
+      <Cart className="w-10 h-10" />
+      <p className="absolute -top-2 -right-2 bg-blue-500 font-bold text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+        {totalQuantity}
+      </p>
     </div>
   );
 };
