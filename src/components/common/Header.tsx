7dd1763f66646f4a054c8e4fa30d18f758a20490
FrontEndDevo <mylinkacco@gmail.com>
2026-09-02 20:06:18 +0300

    Style the app logo and name

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import ShoppingCart from "@/components/ecommerce/ShoppingCart";
-
+import Shopster from "@/assets/Shopster.svg?react";
 const Header = () => {
   return (
     <header>
       <div className="flex justify-between items-center mb-4">
-        eCommerce App
+        <div className="flex justify-center items-center flex-col">
+          <Shopster className="w-10 h-10 rotate-3" />
+          <h1 className="italic text-xl font-semibold text-mist-900">
+            Shopster
+          </h1>
+        </div>
         <ShoppingCart />
       </div>
 
