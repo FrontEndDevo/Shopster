00ed4b1940bbd0ee7d9c1f305e0d21714391e874
FrontEndDevo <mylinkacco@gmail.com>
2026-09-02 13:31:01 +0300

    Handle add to cart button

--- src/components/ecommerce/Product.tsx
+++ src/components/ecommerce/Product.tsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
 import type { TProducts } from "../../types/products";
+import { useAppDispatch } from "../../store/hooks";
+import { addToCart } from "../../store/cart/cartSlice";
 
-const Product = ({ id, title, img, price, cat_prefix }: TProducts) => {
+const Product = ({ id, title, img, price }: TProducts) => {
+  const dispatch = useAppDispatch();
+
+  const handleAddToCart = () => {
+    dispatch(addToCart(id));
+  };
+
   return (
     <Link to="/">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <p>{price}</p>
-      <button>Add to cart</button>
+      <button onClick={handleAddToCart}>Add to cart</button>
     </Link>
   );
 };
