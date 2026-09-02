ae4c38ab20ec332df197d32fe0361c0e01136dca
FrontEndDevo <mylinkacco@gmail.com>
2026-09-02 22:22:15 +0300

    Create Cart Page

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -5,16 +5,17 @@ import Products from "@/pages/Products";
 import About from "@/pages/About";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import Cart from "@/pages/Cart";
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
       <Route path="categories" element={<Categories />} />
       <Route path="products" element={<Products />} />
       <Route path="about" element={<About />} />
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
-      <Route errorElement={<h1>Error</h1>} />
     </Routes>
   );
 };
