3f9e34bc2411eebcfb60f66581a1c6ac146902ee
FrontEndDevo <mylinkacco@gmail.com>
2026-09-02 20:08:31 +0300

    Edit the type of response (response.data) -> (response.data.data)

--- src/store/actions/actGetCategories.ts
+++ src/store/actions/actGetCategories.ts
@@ -2,20 +2,20 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import type { TCategory } from "@/types/category";
 
+const API_CATEGORIES_URL = import.meta.env.VITE_API_CATEGORIES_URL;
+
+type TCategoryResponse = {
+  data: TCategory[];
+};
+
 const actGetCategories = createAsyncThunk(
   "categories/actGetCategories",
   async (_, thunkAPI) => {
     const { rejectWithValue } = thunkAPI;
 
     try {
-      const response = await axios.get<TCategory[]>("");
-
-      // return response.data;
-      return [
-        { title: "men", img: "" },
-        { title: "women", img: "" },
-        { title: "kids", img: "" },
-      ];
+      const response = await axios.get<TCategoryResponse>(API_CATEGORIES_URL);
+      return response.data.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
         return rejectWithValue(error.response?.data.message || error.message);
