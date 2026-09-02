13a21086b43490fab2577d9f0d8eff87e49ffe63
FrontEndDevo <mylinkacco@gmail.com>
2026-09-02 13:28:06 +0300

    Handling redux persist in the redux store

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -1,11 +1,56 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import categories from "./categoriesSlice";
 import products from "./productsSlice";
-const store = configureStore({
-  reducer: {
-    categories,
-    products,
+import cart from "./cart/cartSlice";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+// 1. Explicitly import WebStorage from redux-persist
+import type { WebStorage } from "redux-persist";
+
+// 2. Type your object as WebStorage
+const storage: WebStorage = {
+  getItem: (key: string): Promise<string | null> => {
+    return Promise.resolve(localStorage.getItem(key));
   },
+  setItem: (key: string, value: string): Promise<void> => {
+    localStorage.setItem(key, value);
+    return Promise.resolve();
+  },
+  removeItem: (key: string): Promise<void> => {
+    localStorage.removeItem(key);
+    return Promise.resolve();
+  },
+};
+
+const rootPersistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  categories,
+  products,
+  cart,
+});
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 // Infer the 'RootState' and 'AppDispatch' types from the store itself
@@ -13,4 +58,6 @@ export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
