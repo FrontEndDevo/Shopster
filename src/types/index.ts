1cd392e50e207870d499e92033778a99a92f18f3
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 16:13:01 +0300

    Import and Export types from the main file index

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+import { TLoading } from "./shared.types";
+import { type TCategory, type TCategoryResponse } from "./category.types";
+import { type TProducts, type TProductsResponse } from "./products.types";
+
+export {
+  type TLoading,
+  type TCategory,
+  type TCategoryResponse,
+  type TProducts,
+  type TProductsResponse,
+};
