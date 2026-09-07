838aa444f32d9b5b5f1c474d6e57104f7508bb16
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 13:05:14 +0300

    Finishing wishlist slice to store the wishlist products for each user

---
+++ src/store/wishlist/wishlistSlice.ts
@@ -0,0 +1,74 @@
+import { createSlice } from "@reduxjs/toolkit";
+import actWishlistToggle from "../actions/actWishlistToggle";
+import type { TLoading } from "@/types/shared";
+import type { TProducts } from "@/types/products";
+import actGetWishlist from "../actions/actGetWishlist";
+
+type TWishlistSlice = {
+  productsIds: number[];
+  productsWithFullInfo: TProducts[];
+  loading: TLoading;
+
+  error: null | string;
+};
+
+const initialState: TWishlistSlice = {
+  productsIds: [],
+  productsWithFullInfo: [],
+  loading: "idle",
+  error: null,
+};
+
+const wishlistSlice = createSlice({
+  name: "wishlist",
+  initialState,
+  reducers: {
+    clearWishlist: (state) => {
+      state.productsWithFullInfo = [];
+    },
+  },
+  extraReducers: (builder) => {
+    // Add & Remove a product from wishlist:
+    builder.addCase(actWishlistToggle.pending, (state) => {
+      state.error = null;
+    });
+    builder.addCase(actWishlistToggle.fulfilled, (state, action) => {
+      if (action.payload?.type === "add") {
+        state.productsIds.push(action.payload.id);
+      } else {
+        state.productsIds = state.productsIds.filter(
+          (el) => el !== action.payload?.id,
+        );
+        state.productsWithFullInfo = state.productsWithFullInfo.filter(
+          (el) => el.id !== action.payload?.id,
+        );
+      }
+      state.error = null;
+    });
+    builder.addCase(actWishlistToggle.rejected, (state, action) => {
+      if (action.payload && typeof action.payload === "string")
+        state.error = action.payload;
+    });
+
+    // Get Wishlist products:
+    builder.addCase(actGetWishlist.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    });
+    builder.addCase(actGetWishlist.fulfilled, (state, action) => {
+      state.loading = "succeeded";
+      state.productsWithFullInfo = action.payload;
+    });
+    builder.addCase(actGetWishlist.rejected, (state, action) => {
+      state.loading = "failed";
+      if (action.payload && typeof action.payload === "string")
+        state.error = action.payload;
+    });
+  },
+});
+
+export const { clearWishlist } = wishlistSlice.actions;
+
+export { actWishlistToggle, actGetWishlist };
+
+export default wishlistSlice.reducer;
