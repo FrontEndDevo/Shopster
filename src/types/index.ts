f6c3b0c3627ceaa18076a8e211a673bf770af798
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 16:31:23 +0300

    Create a typescript predicate to handle the logic of error in slices

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,12 @@
 import { TLoading } from "./shared.types";
 import { type TCategory, type TCategoryResponse } from "./category.types";
 import { type TProducts, type TProductsResponse } from "./products.types";
-
+import { isString } from "./guards";
 export {
   type TLoading,
   type TCategory,
   type TCategoryResponse,
   type TProducts,
   type TProductsResponse,
+  isString,
 };
