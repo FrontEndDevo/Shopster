9f4c9416355575cd9679cffe1bc97500f9096cea
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 17:41:55 +0300

    Fix import type of TLoading

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import { TLoading } from "./shared.types";
+import type { TLoading } from "./shared.types";
 import { type TCategory, type TCategoryResponse } from "./category.types";
 import { type TProducts, type TProductsResponse } from "./products.types";
 import { isString } from "./guards";
