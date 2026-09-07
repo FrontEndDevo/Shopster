6e5f42f36adef198665238bb33fb402858058f23
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 13:23:45 +0300

    Seperate the favorite icon to a component to handle the big logic of dealing with the server

---
+++ src/components/feedback/FavoriteButton.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Favorite from "@/assets/favorite.svg?react";
+import { useAppDispatch } from "@/store/hooks";
+import { actWishlistToggle } from "@/store/wishlist/wishlistSlice";
+
+type TFavoriteButtonProps = {
+  id: number;
+  favorite?: boolean;
+};
+
+const FavoriteButton = ({ id, favorite }: TFavoriteButtonProps) => {
+  const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log(isLoading);
+
+  const handleFavoriteProduct = () => {
+    setIsLoading(true);
+    dispatch(actWishlistToggle({ id, type: favorite ? "remove" : "add" }))
+      .unwrap()
+      .then(() => setIsLoading(false))
+      .catch(() => setIsLoading(false));
+  };
+
+  if (isLoading)
+    return (
+      <div className="w-10 h-10 lg:h-6 lg:w-6 absolute top-3 right-3 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
+    );
+
+  return (
+    <Favorite
+      onClick={handleFavoriteProduct}
+      className={`w-10 h-10 lg:h-7 lg:w-7 absolute top-3 right-3 transition duration-200 hover:cursor-pointer hover:text-red-600 ${favorite ? "text-red-600" : ""}`}
+    />
+  );
+};
+
+export default FavoriteButton;
