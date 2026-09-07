874f818ce1646f803f17aa8b3930ac5d80cd715b
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 14:02:46 +0300

    Handle export and import our custom hooks easily

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+import useWishlist from "./useWishlist";
+import useCart from "./useCart";
+import useProducts from "./useProducts";
+import useCategories from "./useCategories";
+
+export { useWishlist, useCart, useProducts, useCategories };
