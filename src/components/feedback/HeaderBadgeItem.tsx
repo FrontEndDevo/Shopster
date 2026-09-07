cfecf0c7daf894dc70ddafc942bcb2e0b330201a
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 13:24:42 +0300

    Create a common component to handle the wishlist and cart (name, icon, and total quantity of items in each of them)

---
+++ src/components/feedback/HeaderBadgeItem.tsx
@@ -0,0 +1,35 @@
+import { memo } from "react";
+import { useNavigate } from "react-router-dom";
+
+type THeaderBadgeItemProps = {
+  label: string;
+  count: number;
+  icon: React.ComponentType<{ className?: string }>;
+};
+
+const HeaderBadgeItem = memo(
+  ({ label, count, icon: Icon }: THeaderBadgeItemProps) => {
+    const navigate = useNavigate();
+
+    return (
+      <div
+        className="relative flex items-center gap-2 flex-row-reverse cursor-pointer group"
+        onClick={() => navigate(`/${label}`)}
+      >
+        <Icon className="w-8 h-8 transition-transform duration-200 hover:scale-110" />
+
+        {count !== 0 && (
+          <p
+            key={count}
+            className="absolute -top-2 -right-2 bg-blue-500 font-bold text-white text-xs rounded-full w-5 h-5 flex items-center justify-center animate-bounce"
+          >
+            {count}
+          </p>
+        )}
+        <p className="text-base capitalize">{label}</p>
+      </div>
+    );
+  },
+);
+
+export default HeaderBadgeItem;
