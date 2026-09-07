21c2b108bf147f201ae3d8251d354de79a22fb03
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 13:18:16 +0300

    Almost finish the logic and JSX of Cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,5 +1,48 @@
+import Heading from "@/components/common/Heading";
+import CartItemList from "@/components/ecommerce/CartItem/CartItemList";
+import CartTotalPrice from "@/components/ecommerce/CartItem/CartTotalPrice";
+import Loading from "@/components/feedback/Loading";
+import { actGetCartProductsByIDs, clearCart } from "@/store/cart/cartSlice";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { useEffect } from "react";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const dispatch = useAppDispatch();
+  const { items, productsWithFullInfo, loading, error } = useAppSelector(
+    (state) => state.cart,
+  );
+  useEffect(() => {
+    dispatch(actGetCartProductsByIDs());
+
+    return () => {
+      dispatch(clearCart());
+    };
+  }, [dispatch]);
+
+  const products = productsWithFullInfo.map((product) => ({
+    ...product,
+    amount: items[product.id],
+  }));
+
+  return (
+    <>
+      <Heading title="Your cart" />
+      <Loading status={loading} error={error}>
+        {products.length > 0 ? (
+          <div className="my-10 mx-5 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <div className="lg:col-span-2">
+              <CartItemList products={products} />
+            </div>
+            <div className="lg:col-span-1">
+              <CartTotalPrice products={products} />
+            </div>
+          </div>
+        ) : (
+          <p className="text-lg">Add some products to the cart</p>
+        )}
+      </Loading>
+    </>
+  );
 };
 
 export default Cart;
