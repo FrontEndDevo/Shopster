da7e4a01d937624480adefb8d7508f2a12f343be
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 13:12:20 +0300

    Handle Categories page and set the Heading component for it

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -4,13 +4,13 @@ import { actGetCategories } from "@/store/categoriesSlice";
 import Category from "@/components/ecommerce/Category";
 import Loading from "@/components/feedback/Loading";
 import RenderList from "@/components/common/RenderList";
+import Heading from "@/components/common/Heading";
 
 const Categories = () => {
   const dispatch = useAppDispatch();
   const { records, loading, error } = useAppSelector(
     (state) => state.categories,
   );
-  console.log(records);
 
   useEffect(() => {
     // Check first if I have the categories already or not, not to send a request again.
@@ -20,18 +20,21 @@ const Categories = () => {
   }, [dispatch, records]);
 
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 xl:grid-cols-4 gap-8 my-6">
-      <Loading status={loading} error={error}>
-        <RenderList
-          records={records}
-          handleRenderList={(record) => (
-            <Category key={record.id} {...record} />
-          )}
-        />
+    <>
+      <Heading title="Categories" />
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 xl:grid-cols-4 gap-8 my-6">
+        <Loading status={loading} error={error}>
+          <RenderList
+            records={records}
+            handleRenderList={(record) => (
+              <Category key={record.id} {...record} />
+            )}
+          />
 
-        {records.length > 0 ? "" : <p>There are no categories.</p>}
-      </Loading>
-    </div>
+          {records.length > 0 ? "" : <p>There are no categories.</p>}
+        </Loading>
+      </div>
+    </>
   );
 };
 
