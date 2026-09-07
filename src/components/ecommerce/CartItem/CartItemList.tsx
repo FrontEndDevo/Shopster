4708a5edd0081d2f49553b8f14ec1abc6a815b55
FrontEndDevo <mylinkacco@gmail.com>
2026-09-07 13:19:20 +0300

    A component to render the titles of products sections and render the products too

---
+++ src/components/ecommerce/CartItem/CartItemList.tsx
@@ -0,0 +1,21 @@
+import type { TProducts } from "@/types/products";
+import CartItem from "./CartItem";
+
+export type TCartItemListProps = { products: TProducts[] };
+
+const CartItemList = ({ products }: TCartItemListProps) => {
+  return (
+    <div>
+      <div className="border-b py-4 grid grid-cols-12 items-center font-semibold">
+        <p className="col-span-8 md:col-span-8 text-left">Product</p>
+        <p className="col-span-3 md:col-span-2 md:block hidden">Quantity</p>
+        <p className="col-span-1 md:block hidden">Subtotal</p>
+      </div>
+      {products.map((el) => (
+        <CartItem key={el.id} {...el} />
+      ))}
+    </div>
+  );
+};
+
+export default CartItemList;
