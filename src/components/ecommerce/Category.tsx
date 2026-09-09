744eb4ebd451b0d4fa6acddae47fb44216a7f7e7
FrontEndDevo <mylinkacco@gmail.com>
2026-09-09 12:01:46 +0300

    Fix category image

--- src/components/ecommerce/Category.tsx
+++ src/components/ecommerce/Category.tsx
@@ -4,11 +4,11 @@ import type { TCategory } from "@/types/category.types";
 const Category = ({ name, image, slug }: TCategory) => {
   return (
     <Link
-      className="transform transition duration-300 hover:cursor-pointer hover:scale-105 hover:border-blue-400 border-2 bg-white rounded-md overflow-hidden border-gray-300 flex flex-col gap-2"
+      className="transform transition duration-300 hover:cursor-pointer hover:scale-105 hover:border-blue-400 border-2 bg-white rounded-md overflow-hidden border-gray-300 w-fit flex flex-col gap-2"
       to={`/categories/${slug}`}
     >
       <img
-        className="w-full h-full rounded-t-sm object-cover"
+        className="w-60 h-w-60 rounded-t-sm object-cover flex-1"
         src={image}
         alt={name}
       />
