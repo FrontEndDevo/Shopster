8b65a2b93f5912fc4f034abadb6b9c2bbc380878
FrontEndDevo <mylinkacco@gmail.com>
2026-09-09 12:05:01 +0300

    Almost finish login page except handling requests to the API (Soon)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,50 @@
+import Heading from "@/components/common/Heading";
+import Input from "@/components/forms/Input";
+import { LoginSchema, type TLoginInputs } from "@/validation/LoginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TLoginInputs>({
+    mode: "onBlur",
+    resolver: zodResolver(LoginSchema),
+  });
+
+  const handleLoginForm: SubmitHandler<TLoginInputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Heading title="Log in"/>
+      <form onSubmit={handleSubmit(handleLoginForm)}>
+        <Input
+          label="Email"
+          type="email"
+          name="email"
+          register={register}
+          error={errors.email?.message as string}
+        />
+        <Input
+          label="Password"
+          type="password"
+          name="password"
+          register={register}
+          error={errors.password?.message as string}
+        />
+
+        <button
+          className="font-semibold text-white py-2 px-6 my-2 cursor-pointer bg-blue-600 rounded transition duration-200 hover:bg-blue-800"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </>
+  );
 };
 export default Login;
