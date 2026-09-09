243f8d076a09fb15eee9dec58ad8aded74ff23ac
FrontEndDevo <mylinkacco@gmail.com>
2026-09-09 12:22:58 +0300

    Create error type to use it

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,11 @@
-import type { TLoading } from "./shared.types";
+import type { TLoading, TError } from "./shared.types";
 import { type TCategory, type TCategoryResponse } from "./category.types";
 import { type TProducts, type TProductsResponse } from "./products.types";
 import { type TEmailAvailabilityStatus } from "./emailAvailability.types";
 import { isString } from "./guards";
 export {
   type TLoading,
+  type TError,
   type TCategory,
   type TCategoryResponse,
   type TProducts,
