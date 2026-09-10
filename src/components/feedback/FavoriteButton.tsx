ae4cde94dba762a26f56e793a9ddefb5f9aacd0d
FrontEndDevo <mylinkacco@gmail.com>
2026-09-10 14:11:01 +0300

    Simple popup model to handle product favorite without log in

--- src/components/feedback/FavoriteButton.tsx
+++ src/components/feedback/FavoriteButton.tsx
@@ -2,22 +2,33 @@ import { useState } from "react";
 import Favorite from "@/assets/favorite.svg?react";
 import { useAppDispatch } from "@/store/hooks";
 import { actWishlistToggle } from "@/store/wishlist/wishlistSlice";
+import PopupModel from "./PopupModel";
 
 type TFavoriteButtonProps = {
   id: number;
   favorite?: boolean;
+  isAuthenticated?: boolean;
 };
 
-const FavoriteButton = ({ id, favorite }: TFavoriteButtonProps) => {
+const FavoriteButton = ({
+  id,
+  favorite,
+  isAuthenticated,
+}: TFavoriteButtonProps) => {
   const dispatch = useAppDispatch();
   const [isLoading, setIsLoading] = useState(false);
+  const [showModel, setShowModel] = useState(false);
 
   const handleFavoriteProduct = () => {
-    setIsLoading(true);
-    dispatch(actWishlistToggle({ id, type: favorite ? "remove" : "add" }))
-      .unwrap()
-      .then(() => setIsLoading(false))
-      .catch(() => setIsLoading(false));
+    if (isAuthenticated) {
+      setIsLoading(true);
+      dispatch(actWishlistToggle({ id, type: favorite ? "remove" : "add" }))
+        .unwrap()
+        .then(() => setIsLoading(false))
+        .catch(() => setIsLoading(false));
+    } else {
+      setShowModel(true);
+    }
   };
 
   if (isLoading)
@@ -26,10 +37,13 @@ const FavoriteButton = ({ id, favorite }: TFavoriteButtonProps) => {
     );
 
   return (
-    <Favorite
-      onClick={handleFavoriteProduct}
-      className={`w-10 h-10 lg:h-7 lg:w-7 absolute top-3 right-3 transition duration-200 hover:cursor-pointer hover:text-red-600 ${favorite ? "text-red-600" : ""}`}
-    />
+    <>
+      {showModel && <PopupModel closeModel={() => setShowModel(false)} />}
+      <Favorite
+        onClick={handleFavoriteProduct}
+        className={`w-10 h-10 lg:h-7 lg:w-7 absolute top-3 right-3 transition duration-200 hover:cursor-pointer hover:text-red-600 ${favorite ? "text-red-600" : ""}`}
+      />
+    </>
   );
 };
 
---
+++ src/components/feedback/PopupModel.tsx
@@ -0,0 +1,43 @@
+import { createPortal } from "react-dom";
+import { useNavigate } from "react-router-dom";
+
+const PopupModel = ({ closeModel }: { closeModel: () => void }) => {
+  const navigate = useNavigate();
+  return createPortal(
+    <div className="fixed top-1/12 right-2/12 md:right-1/4 lg:right-1/3 xl:right-2/5 h-fit w-fit py-6 px-20 z-50 rounded-lg bg-neutral-600 text-white">
+      <div className="relative">
+        <button
+          onClick={() => closeModel()}
+          className="absolute -top-4 -right-16 text-2xl hover:text-red-600 transition duration-150 cursor-pointer p-1"
+        >
+          x
+        </button>
+        <div className="text-center lg:py-4 lg:px-12 ">
+          <div>
+            <p className="text-lg lg:text-2xl font-semibold">Login Required!</p>
+            <p className="my-4 text-sm lg:text-base">
+              You need to login first.
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <button
+            onClick={() => closeModel()}
+            className="rounded-sm bg-red-600 hover:shadow-red-400 hover:shadow-lg transition duration-150 py-2 px-6 font-semibold italic text-lg cursor-pointer"
+          >
+            Close
+          </button>
+          <button
+            onClick={() => navigate("/login")}
+            className="rounded-sm bg-green-600 hover:shadow-green-400 hover:shadow-lg transition duration-150 py-2 px-6 font-semibold italic text-lg cursor-pointer"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>,
+    document.body,
+  );
+};
+
+export default PopupModel;
