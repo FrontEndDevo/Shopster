c8b9034dc72558d366f66e051a199b26a21c9b90
FrontEndDevo <mylinkacco@gmail.com>
2026-09-10 13:05:53 +0300

    Call the total products in the wishlist (count only) when the user log in and go to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,14 @@
+import actGetWishlist from "@/store/actions/actGetWishlist";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { useEffect } from "react";
+
 const Home = () => {
+  const dispatch = useAppDispatch();
+  const { token } = useAppSelector((state) => state.auth);
+  useEffect(() => {
+    if (token) dispatch(actGetWishlist("productsIds"));
+  }, [dispatch, token]);
+
   return <div>Home</div>;
 };
 
