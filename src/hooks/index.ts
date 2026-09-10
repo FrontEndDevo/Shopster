70d0ecafdc117777ed7b905f51bde20c76b69dcc
FrontEndDevo <mylinkacco@gmail.com>
2026-09-10 13:00:27 +0300

    Handle imports for useLogin and useRegister

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,15 @@ import useWishlist from "./useWishlist";
 import useCart from "./useCart";
 import useProducts from "./useProducts";
 import useCategories from "./useCategories";
-
-export { useWishlist, useCart, useProducts, useCategories };
+import useCheckEmailAvailability from "./useCheckEmailAvailability";
+import useLogin from "./useLogin";
+import useRegister from "./useRegister";
+export {
+  useWishlist,
+  useCart,
+  useProducts,
+  useCategories,
+  useCheckEmailAvailability,
+  useLogin,
+  useRegister,
+};
