d29c3e2689e84806bd13e9fc94e2ba52fdc6a38c
FrontEndDevo <mylinkacco@gmail.com>
2026-09-13 21:34:24 +0300

    Finish get cart user products asyncthunk

---
+++ src/store/cart/actions/actGetLoggedUserCart.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import AxiosErrorHandler from "@/utils/AxiosErrorHandler";
+import type { RootState } from "../../redux";
+import type { TCartItemResponse } from "@/types/products.types";
+
+const API_CART_PRODUCT = import.meta.env.VITE_ECOMMERCE_API_VERSION_2;
+
+const actGetLoggedUserCart = createAsyncThunk(
+  "cart/actGetLoggedUserCart",
+  async (_, thunkAPI) => {
+    const { rejectWithValue, getState } = thunkAPI;
+    const { auth } = getState() as RootState;
+
+    try {
+      const response = await axios.get<TCartItemResponse>(
+        `${API_CART_PRODUCT}/cart`,
+        {
+          headers: {
+            token: auth.token,
+          },
+        },
+      );
+
+      return {
+        totalCartPrice: response.data.data.totalCartPrice,
+        products: response.data.data.products,
+      };
+    } catch (error) {
+      return rejectWithValue(AxiosErrorHandler(error));
+    }
+  },
+);
+
+export default actGetLoggedUserCart;
