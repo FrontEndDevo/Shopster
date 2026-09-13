caf34bc1777ebbe095f99f9df21f03b62a4952ba
FrontEndDevo <mylinkacco@gmail.com>
2026-09-14 01:36:35 +0300

    Move the logic of dropdown and Login/Register to ProfileDropdown component

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,25 +1,18 @@
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { useAppSelector } from "@/store/hooks";
 import { Link, useNavigate } from "react-router-dom";
 import Cart from "@/assets/cart.svg?react";
 import Wishlist from "@/assets/wishlist.svg?react";
 import Shopster from "@/assets/Shopster.svg?react";
 import HeaderBadgeItem from "../feedback/HeaderBadgeItem";
 import { getTotalCartQuantitySelector } from "@/store/selectors";
-import { authLogout } from "@/store/auth/authSlice";
+import ProfileDropdown from "../feedback/ProfileDropdown";
 const Header = () => {
-  const dispatch = useAppDispatch();
-  const { user, token } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const { token } = useAppSelector((state) => state.auth);
   const totalQuantity = useAppSelector(getTotalCartQuantitySelector);
   const totalProducts = useAppSelector(
     (state) => state.wishlist.productsIds.length,
   );
-
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    dispatch(authLogout());
-    navigate("/login");
-  };
 
   return (
     <header>
@@ -49,37 +42,13 @@ const Header = () => {
       </div>
 
       <div className="flex justify-between items-center bg-neutral-800 text-white p-2">
-        <ul className="flex gap-2">
+        <ul className="flex gap-2 py-2 rounded-sm">
           <Link to="/">Home</Link>
           <Link to="/categories">Categories</Link>
           <Link to="/products">Products</Link>
           <Link to="/about">About</Link>
         </ul>
-        {token ? (
-          <div className="flex gap-2">
-            <p className="text-base font-semibold">
-              Welcome:{" "}
-              <button
-                className="text-amber-300 animate-pulse transition duration-150 hover:text-amber-500 cursor-pointer"
-                onClick={() => navigate("/profile")}
-              >
-                {user?.name}
-              </button>
-            </p>
-            <span className="w-px h-6 mx-1 bg-white"></span>
-            <button
-              className="text-red-50 hover:text-red-400 transition duration-150 cursor-pointer"
-              onClick={handleLogout}
-            >
-              Log out
-            </button>
-          </div>
-        ) : (
-          <div className="flex gap-2">
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </div>
-        )}
+        <ProfileDropdown />
       </div>
     </header>
   );
