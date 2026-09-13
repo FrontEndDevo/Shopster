c6d7ed5f654b98774381fcc1e2dbb8b5591dbe11
FrontEndDevo <mylinkacco@gmail.com>
2026-09-13 21:31:09 +0300

    Add seperate cart product type

--- src/types/products.types.ts
+++ src/types/products.types.ts
@@ -2,16 +2,39 @@ export type TProducts = {
   id: number;
   title: string;
   slug?: string;
-  description: string;
+  description?: string;
   imageCover: string;
+  images: string[];
   price: number;
   priceAfterDiscount?: number;
   quantity?: number;
-  amount?: number;
   isFavorite?: boolean;
   isAuthenticated?: boolean;
 };
 
 export type TProductsResponse = {
   data: TProducts[];
+};
+
+export type TCartProduct = {
+  count: number;
+  price: number;
+  product: {
+    id: string;
+    title: string;
+    slug: string;
+    priceAfterDiscount?: number;
+    quantity: number;
+    imageCover: string;
+    images: string[];
+  };
+};
+
+export type TCartItem = {
+  totalCartPrice: number;
+  products: TCartProduct[];
+};
+
+export type TCartItemResponse = {
+  data: TCartItem;
 };
