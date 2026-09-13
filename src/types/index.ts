94151295e472d5783700c5aceec9fbbb9b729e48
FrontEndDevo <mylinkacco@gmail.com>
2026-09-13 21:40:11 +0300

    Import and Export cart product types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,11 @@
 import type { TLoading, TError } from "./shared.types";
 import { type TCategory, type TCategoryResponse } from "./category.types";
-import { type TProducts, type TProductsResponse } from "./products.types";
+import {
+  type TProducts,
+  type TProductsResponse,
+  type TCartProduct,
+  type TCartItem,
+} from "./products.types";
 import { type TEmailAvailabilityStatus } from "./emailAvailability.types";
 import { isString } from "./guards";
 export {
@@ -11,5 +16,7 @@ export {
   type TProducts,
   type TProductsResponse,
   type TEmailAvailabilityStatus,
+  type TCartProduct,
+  type TCartItem,
   isString,
 };
