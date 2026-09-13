52cb368bf77027a57645c0efbbc33e5f4d57aa8b
FrontEndDevo <mylinkacco@gmail.com>
2026-09-14 01:36:03 +0300

    Make the footer better

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return <footer>@2026 eCommerce App | All rights reserved.</footer>;
+  return (
+    <footer className="bg-gray-400 text-white">
+      @2026 <span className="text-amber-300 font-semibold">Shopster</span>{" "}
+      eCommerce App | All rights reserved.
+    </footer>
+  );
 };
 
 export default Footer;
