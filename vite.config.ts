f5ac1084dfd27db131c064a2d3a503c74c90cb7a
FrontEndDevo <mylinkacco@gmail.com>
2026-09-14 01:37:18 +0300

    Install flowbite-react to use UI components

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss(), svgr(), flowbiteReact()],
   resolve: {
     tsconfigPaths: true,
   },
