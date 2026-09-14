7cb522cdeb15a7d6cdc4fe684289cf002773b735
FrontEndDevo <mylinkacco@gmail.com>
2026-09-15 02:57:55 +0300

    Convert the add product to wishlist action loading to Spinner.tsx

---
+++ src/components/feedback/Spinner.tsx
@@ -0,0 +1,7 @@
+const Spinner = () => {
+  return (
+    <div className="w-10 h-10 lg:h-6 lg:w-6 absolute top-3 right-3 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
+  );
+};
+
+export default Spinner;
