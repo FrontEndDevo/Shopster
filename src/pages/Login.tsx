4e73ad8267281a2aee30e462600f9ce7023ae3c5
FrontEndDevo <mylinkacco@gmail.com>
2026-09-16 01:36:11 +0300

    Refactor Login and Register components for improved layout consistency

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,37 +13,39 @@ const Login = () => {
   } = useLogin();
 
   return (
-    <>
-      <Heading title="Log in" />
-      <form onSubmit={handleSubmit(handleLoginForm)}>
-        <Input
-          label="Email"
-          type="email"
-          name="email"
-          register={register}
-          error={formErrors.email?.message as string}
-        />
-        <Input
-          label="Password"
-          type="password"
-          name="password"
-          register={register}
-          error={formErrors.password?.message as string}
-        />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-md">
+        <Heading title="Log in" />
+        <form onSubmit={handleSubmit(handleLoginForm)}>
+          <Input
+            label="Email"
+            type="email"
+            name="email"
+            register={register}
+            error={formErrors.email?.message as string}
+          />
+          <Input
+            label="Password"
+            type="password"
+            name="password"
+            register={register}
+            error={formErrors.password?.message as string}
+          />
 
-        <button
-          className="font-semibold text-white py-2 px-6 my-2 cursor-pointer bg-blue-600 rounded transition duration-200 hover:bg-blue-800 disabled:bg-gray-400 disabled:cursor-default"
-          type="submit"
-          disabled={loading === "pending" || loading === "succeeded"}
-        >
-          {loading === "pending" ? "Logining..." : "Login"}
-        </button>
+          <button
+            className="font-semibold text-white py-2 px-6 my-2 cursor-pointer bg-blue-600 rounded transition duration-200 hover:bg-blue-800 disabled:bg-gray-400 disabled:cursor-default"
+            type="submit"
+            disabled={loading === "pending" || loading === "succeeded"}
+          >
+            {loading === "pending" ? "Logining..." : "Login"}
+          </button>
 
-        {error && loading === "failed" && (
-          <p className="text-sm font-semibold text-red-600 my-2">{error}</p>
-        )}
-      </form>
-    </>
+          {error && loading === "failed" && (
+            <p className="text-sm font-semibold text-red-600 my-2">{error}</p>
+          )}
+        </form>
+      </div>
+    </div>
   );
 };
 export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -15,79 +15,81 @@ const Register = () => {
   } = useRegister();
 
   return (
-    <>
-      <Heading title="Register" />
-      <form
-        className="max-w-sm mx-auto"
-        onSubmit={handleSubmit(handleSubmitForm)}
-      >
-        <Input
-          label="First Name"
-          name="firstName"
-          type="text"
-          error={formErrors.firstName?.message as string}
-          register={register}
-        />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-md">
+        <Heading title="Register" />
+        <form
+          className="max-w-sm mx-auto"
+          onSubmit={handleSubmit(handleSubmitForm)}
+        >
+          <Input
+            label="First Name"
+            name="firstName"
+            type="text"
+            error={formErrors.firstName?.message as string}
+            register={register}
+          />
 
-        <Input
-          label="Last Name"
-          name="lastName"
-          type="text"
-          error={formErrors.lastName?.message as string}
-          register={register}
-        />
+          <Input
+            label="Last Name"
+            name="lastName"
+            type="text"
+            error={formErrors.lastName?.message as string}
+            register={register}
+          />
 
-        <Input
-          label="Your email"
-          name="email"
-          type="email"
-          error={formErrors.email?.message as string}
-          register={register}
-          onBlur={handleEmailOnBlur}
-          emailStatus={emailAvailabilityStatus}
-        />
+          <Input
+            label="Your email"
+            name="email"
+            type="email"
+            error={formErrors.email?.message as string}
+            register={register}
+            onBlur={handleEmailOnBlur}
+            emailStatus={emailAvailabilityStatus}
+          />
 
-        <Input
-          label="Password"
-          name="password"
-          type="password"
-          error={formErrors.password?.message as string}
-          register={register}
-        />
+          <Input
+            label="Password"
+            name="password"
+            type="password"
+            error={formErrors.password?.message as string}
+            register={register}
+          />
 
-        <Input
-          label="Confirm Password"
-          name="confirmPasaword"
-          type="password"
-          error={formErrors.confirmPasaword?.message as string}
-          register={register}
-        />
+          <Input
+            label="Confirm Password"
+            name="confirmPasaword"
+            type="password"
+            error={formErrors.confirmPasaword?.message as string}
+            register={register}
+          />
 
-        <div className="flex items-center justify-between">
-          <button
-            className="font-semibold text-white py-2 px-6 my-2 cursor-pointer bg-blue-600 rounded transition duration-200 hover:bg-blue-800 disabled:bg-gray-400 disabled:cursor-default"
-            type="submit"
-            disabled={
-              emailAvailabilityStatus === "checking" ||
-              emailAvailabilityStatus === "notAvailable" ||
-              emailAvailabilityStatus === "failed" ||
-              loading === "pending" ||
-              loading === "succeeded"
-            }
-          >
-            {loading === "pending" ? "Creating account..." : "Sign Up"}
-          </button>
-          {error && loading === "failed" && (
-            <p className="text-sm font-semibold text-red-600 my-2">{error}</p>
+          <div className="flex items-center justify-between">
+            <button
+              className="font-semibold text-white py-2 px-6 my-2 cursor-pointer bg-blue-600 rounded transition duration-200 hover:bg-blue-800 disabled:bg-gray-400 disabled:cursor-default"
+              type="submit"
+              disabled={
+                emailAvailabilityStatus === "checking" ||
+                emailAvailabilityStatus === "notAvailable" ||
+                emailAvailabilityStatus === "failed" ||
+                loading === "pending" ||
+                loading === "succeeded"
+              }
+            >
+              {loading === "pending" ? "Creating account..." : "Sign Up"}
+            </button>
+            {error && loading === "failed" && (
+              <p className="text-sm font-semibold text-red-600 my-2">{error}</p>
+            )}
+          </div>
+          {loading === "succeeded" && (
+            <p className="text-sm font-semibold text-green-600 my-2">
+              Your account was successfully created, please login now
+            </p>
           )}
-        </div>
-        {loading === "succeeded" && (
-          <p className="text-sm font-semibold text-green-600 my-2">
-            Your account was successfully created, please login now
-          </p>
-        )}
-      </form>
-    </>
+        </form>
+      </div>
+    </div>
   );
 };
 export default Register;
