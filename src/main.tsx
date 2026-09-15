7a542b405b7d864c055e59979e9711094572f2c6
FrontEndDevo <mylinkacco@gmail.com>
2026-09-16 01:37:09 +0300

    Replace App component with MainLayout in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store/redux.ts";
 import { PersistGate } from "redux-persist/integration/react";
+import MainLayout from "./layouts/MainLayout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <MainLayout />
       </PersistGate>
     </Provider>
   </BrowserRouter>,
