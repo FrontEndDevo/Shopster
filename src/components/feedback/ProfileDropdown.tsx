9fb8b2f9bf0b0b1567903da4084a15acd77d0ff5
FrontEndDevo <mylinkacco@gmail.com>
2026-09-20 00:47:31 +0300

    Refactor ProfileDropdown component to enhance user experience with improved layout and user icon integration

--- src/components/feedback/ProfileDropdown.tsx
+++ src/components/feedback/ProfileDropdown.tsx
@@ -8,6 +8,7 @@ import {
 } from "flowbite-react";
 import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
+import UserIcon from "@/assets/user.svg?react";
 
 const ProfileDropdown = () => {
   const dispatch = useAppDispatch();
@@ -21,15 +22,28 @@ const ProfileDropdown = () => {
   };
 
   return (
-    <>
+    <div className="flex md:order-2">
       {token ? (
-        <Dropdown label="Dropdown button" color={"gray"}>
+        <Dropdown
+          arrowIcon={false}
+          inline
+          renderTrigger={() => (
+            <button
+              type="button"
+              className="p-1 cursor-pointer text-gray-500 rounded-full hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white transition-colors focus:outline-none"
+              aria-label="User menu"
+            >
+              <UserIcon className="w-8 h-8" />
+            </button>
+          )}
+        >
           <DropdownHeader>
             <h3 className="block text-sm text-amber-300 animate-pulse transition duration-150 font-bold capitalize text-start">
               {user?.name}
             </h3>
             <p className="block truncate text-sm font-medium">{user?.email}</p>
           </DropdownHeader>
+
           <DropdownItem onClick={() => navigate("/profile")}>
             Profile
           </DropdownItem>
@@ -40,12 +54,17 @@ const ProfileDropdown = () => {
           <DropdownItem onClick={handleLogout}>Sign out</DropdownItem>
         </Dropdown>
       ) : (
-        <div className="flex gap-2">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+        <div className="flex items-center gap-4 text-white font-semibold">
+          <Link className="hover:text-amber-300" to="/login">
+            Login
+          </Link>
+          <span className="w-0.5 h-10 bg-gray-600" />
+          <Link className="hover:text-amber-300" to="/register">
+            Register
+          </Link>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
