703124acb8cbeac88136ecb96f501b8f97b61356
FrontEndDevo <mylinkacco@gmail.com>
2026-09-24 12:48:55 +0300

    Add actGetUserAddresses action for fetching user addresses from API

---
+++ src/store/actions/actGetUserAddresses.ts
@@ -0,0 +1,28 @@
+import AxiosErrorHandler from "@/utils/AxiosErrorHandler";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import type { RootState } from "../redux";
+
+const ADDRESS_API = import.meta.env.VITE_ECOMMERCE_API;
+
+const actGetUserAddresses = createAsyncThunk(
+  "address/actGetUserAddresses",
+  async (_, thunkAPI) => {
+    const { rejectWithValue, getState } = thunkAPI;
+    const { auth } = getState() as RootState;
+
+    try {
+      const response = await axios.get(`${ADDRESS_API}/addresses`, {
+        headers: {
+          token: auth.token,
+        },
+      });
+
+      return response.data.data;
+    } catch (error) {
+      return rejectWithValue(AxiosErrorHandler(error));
+    }
+  },
+);
+
+export default actGetUserAddresses;
