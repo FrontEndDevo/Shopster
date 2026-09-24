1292da3cde9c7285762ee7c39285252ec644a7d6
FrontEndDevo <mylinkacco@gmail.com>
2026-09-24 12:49:49 +0300

    Enhance CartTotalPrice component with address retrieval and order handling

--- src/components/ecommerce/CartItem/CartTotalPrice.tsx
+++ src/components/ecommerce/CartItem/CartTotalPrice.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { TCartItem } from "@/types";
 import UserAddress from "@/components/forms/UserAddress";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import actPlaceOrder from "@/store/actions/actPlaceOrder";
+import { actPlaceOrder, ordersInit } from "@/store/orders/ordersSlice";
+import { actClearUserCart } from "@/store/cart/cartSlice";
+import { useNavigate } from "react-router-dom";
+import actGetUserAddresses from "@/store/actions/actGetUserAddresses";
 
 type TCartItemsProps = { productsFull: TCartItem };
 
@@ -10,16 +13,27 @@ const CartTotalPrice = ({ productsFull }: TCartItemsProps) => {
   const [showModal, setShowModal] = useState(false);
   const dispatch = useAppDispatch();
   const { shippingAddresses } = useAppSelector((state) => state.address);
+  const { loading, error } = useAppSelector((state) => state.orders);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(actGetUserAddresses());
+  }, [dispatch]);
 
   const handlePlaceOrder = () => {
     // If we have no addresses for the user, Open the model to get one.
     if (shippingAddresses.length === 0) {
       setShowModal(true);
-    } else {
-      // Do something here:
-      console.log(`something here`);
-      dispatch(actPlaceOrder());
+      return;
     }
+
+    dispatch(actPlaceOrder())
+      .unwrap()
+      .then(() => {
+        dispatch(actClearUserCart());
+        dispatch(ordersInit());
+      })
+      .finally(() => navigate("/orders"));
   };
 
   // I used useMemo to prevent calling this heavy function again in vain.
@@ -45,12 +59,17 @@ const CartTotalPrice = ({ productsFull }: TCartItemsProps) => {
         </div>
 
         <button
-          disabled={showModal}
+          disabled={showModal || loading == "pending"}
           className="font-semibold text-lg bg-blue-500 py-1 px-3 mx-auto rounded text-white transition duration-100 hover:bg-blue-600 cursor-pointer disabled:bg-blue-300 disabled:cursor-default"
           onClick={handlePlaceOrder}
         >
           Procced to checkout
         </button>
+        {error && (
+          <p className="text-sm text-center text-red-400">
+            Failed to place order: {error}
+          </p>
+        )}
       </div>
       {showModal && <UserAddress closeModal={() => setShowModal(false)} />}
     </>
